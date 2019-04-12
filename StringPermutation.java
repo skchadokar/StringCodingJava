ad851a579459eeff5e67eca6b2991e25d0692123
skchadokar <49568331+skchadokar@users.noreply.github.com>
2019-04-12 10:53:19 -0700

    Update StringPermutation.java

--- StringPermutation.java
+++ StringPermutation.java
@@ -1,1 +1,20 @@
-hi
+
+public class StringPermutation {
+  
+ public static void main(String[] args) {
+   
+   	new PermutaionOfString().permutation("","satish");
+ }
+
+  private  void permutation(String prefix, String str) {
+	    int n = str.length();
+	    if (n == 0) System.out.println(prefix);//print all permutation
+	    else {
+	        for (int i = 0; i < n; i++) {
+	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+	        }
+	   }
+	}
+  
+  
+}
