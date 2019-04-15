9a8c28fa3f13f6d5e1c712909dd2187ab257c9fd
skchadokar <49568331+skchadokar@users.noreply.github.com>
2019-04-15 09:47:09 -0700

    Create LongestSubString.java

---
+++ LongestSubString.java
@@ -0,0 +1,33 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubString {
+
+	public static void main(String[] args) {
+	System.out.println(getUniqueCharacterSubstring("xabbabdd")); 
+		
+	}
+	
+	static String getUniqueCharacterSubstring(String aInputString) {
+		
+		int mSlowPointer = 0,mFastPointer=0;
+		String mOutPut ="";
+		Map<Character,Integer> mVisitedMap=  new HashMap<>();
+		while(mSlowPointer<aInputString.length() && mFastPointer<aInputString.length()) {
+			char mCurrentChar = aInputString.charAt(mFastPointer);
+			if(mVisitedMap.containsKey(mCurrentChar)) {
+				mSlowPointer=Math.max(mVisitedMap.get(mCurrentChar)+1,mSlowPointer);//very imp 
+			}
+				mVisitedMap.put(mCurrentChar, mFastPointer);
+			
+				if(mOutPut.length()<Math.abs(mFastPointer-mSlowPointer)+1) {
+					mOutPut = aInputString.substring(mSlowPointer,mFastPointer+1);//new String
+			}
+			mFastPointer++;
+		}
+		return mOutPut;
+		
+	}
+
+}
